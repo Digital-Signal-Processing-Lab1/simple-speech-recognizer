e92bfe3f656af69dc4bf515c6d606e5fa2a94714
wind2375like <wind2375like@163.com>
2020-10-06 18:21:51 +0800

    增加滤波，提高门限检测准确性

--- VAD.py
+++ VAD.py
@@ -8,6 +8,7 @@ import json
 import zipfile
 import matplotlib.pyplot as plt
 import pickle
+import scipy.signal as signal
 
 # 将record.wav输入信号 采样 切割 转化成若干processedi.pcm文件
 
@@ -17,8 +18,10 @@ import pickle
 
 
 def sgn(x):
-    if x >= 0: return 1
-    else: return -1
+    if x >= 0:
+        return 1
+    else:
+        return -1
 
 def calEnergy(frames, N):
     """ 返回每一帧的短时能量energy
@@ -44,36 +47,36 @@ def calZeroCrossingRate(frames, N):
         N: 一帧采样点个数
     """
     zerocrossingrate = []
-    zerocrossingrate = np.sum(np.abs(frames[:, 1:N-1]-frames[:, 0:N-2]), axis=1)
+    zerocrossingrate = np.sum(
+        np.abs(frames[:, 1:N-1]-frames[:, 0:N-2]), axis=1)
     return zerocrossingrate
 
-
-# [TODO: detectEndPoint 有大量重复的值，且排布不是升序]
 def detectEndPoint(wave_data, energy, zerocrossingrate):
     """ 利用短时能量/短时幅度，短时过零率，使用双门限法进行端点检测
         返回端点对应的帧序号endpoint0
         wave_data: 向量存储的语音信号
         energy: 一帧采样点个数
     """
-
-    # [TODO: 继续调试TH，TL，T0的值]
-    TH = np.mean(energy)                                    # 较高能量门限
-    TL = np.mean(energy[:5]) * 0.999 + TH * 0.001           # 较低能量门限
-    T0 = np.mean(zerocrossingrate[:5]) * 0.999 + TL * 0.001 # 过零率门限
+    smooth_energy = signal.savgol_filter(energy, 99, 1)             # 利用savgol滤波器对能量和过零率进行平滑
+    smooth_zcr = signal.savgol_filter(zerocrossingrate, 99, 1)
+    
+    TH = np.mean(smooth_energy)                                     # 较高能量门限
+    TL = np.mean(smooth_energy[:5]) * 0.9 + TH * 0.1                # 较低能量门限
+    T0 = np.mean(smooth_zcr[:5]) * 0.8 + np.max(smooth_zcr) * 0.2   # 过零率门限
     endpointH = []  # 存储高能量门限 端点帧序号
     endpointL = []  # 存储低能量门限 端点帧序号
     endpoint0 = []  # 存储过零率门限 端点帧序号
 
     # 先利用较高能量门限 TH 筛选语音段
     flag = 0
-    for i in range(len(energy)):
+    for i in range(len(smooth_energy)):
         # 左端点判断
-        if flag == 0 and energy[i] >= TH:
+        if flag == 0 and smooth_energy[i] >= TH:
             endpointH.append(i)
             flag = 1
 
         # 右端点判断
-        if flag == 1 and energy[i] < TH:
+        if flag == 1 and smooth_energy[i] < TH:
             endpointH.append(i)
             flag = 0
 
@@ -83,29 +86,29 @@ def detectEndPoint(wave_data, energy, zerocrossingrate):
 
         # 对右端点向右搜索
         if j % 2 == 1:
-            while i < len(energy) and energy[i] >= TL:
+            while i < len(smooth_energy) and smooth_energy[i] >= TL:
                 i = i + 1
             endpointL.append(i)
 
         # 对左端点向左搜索
         else:
-            while i > 0 and energy[i] >= TL:
+            while i > 0 and smooth_energy[i] >= TL:
                 i = i - 1
             endpointL.append(i)
 
     # 最后利用过零率门限 T0 得到最终语音段
-    for j in range(len(endpointL)) :
+    for j in range(len(endpointL)):
         i = endpointL[j]
 
         # 对右端点向右搜索
         if j % 2 == 1:
-            while i < len(zerocrossingrate) and zerocrossingrate[i] >= T0:
+            while i < len(smooth_zcr) and smooth_zcr[i] >= T0:
                 i = i + 1
             endpoint0.append(i)
 
         # 对左端点向左搜索
         else:
-            while i > 0 and zerocrossingrate[i] >= T0:
+            while i > 0 and smooth_zcr[i] >= T0:
                 i = i - 1
             endpoint0.append(i)
 
@@ -126,10 +129,11 @@ def addWindow(wave_data, N, M, winfunc):
     else:
         num_frame = int(np.ceil((1.0*wav_length-N)/inc + 1))
     pad_length = int((num_frame-1)*inc+N)               # 所有帧加起来铺平后长度
-    zeros = np.zeros((pad_length-wav_length, 1)) # 不够的长度用0填补
-    pad_wavedata = np.concatenate((wave_data, zeros)) # 填补后的信号
+    zeros = np.zeros((pad_length-wav_length, 1))  # 不够的长度用0填补
+    pad_wavedata = np.concatenate((wave_data, zeros))  # 填补后的信号
     indices = np.tile(np.arange(0, N), (num_frame, 1)) + \
-              np.tile(np.arange(0, num_frame*inc, inc), (N, 1)).T   # 用来对pad_wavedata进行抽取，得到num_frame*N的矩阵
+        np.tile(np.arange(0, num_frame*inc, inc), (N, 1)
+                ).T   # 用来对pad_wavedata进行抽取，得到num_frame*N的矩阵
     frames = pad_wavedata[indices].reshape(num_frame, N)            # 得到帧信号矩阵
     window = np.tile(winfunc, (num_frame, 1))
     return window * frames, num_frame                   # 加窗
@@ -146,11 +150,11 @@ def readWav(filename):
     str_data = f.readframes(nframes)
 
     # 转成二字节数组形式（每个采样点占两个字节）
-    wave_data = np.fromstring(str_data, dtype = np.short)
-    wave_data = np.reshape(wave_data, [nframes, nchannels]) # 转化为向量形式
+    wave_data = np.fromstring(str_data, dtype=np.short)
+    wave_data = np.reshape(wave_data, [nframes, nchannels])  # 转化为向量形式
     if nchannels == 2:
         wave_data = np.mean(wave_data, axis=1).reshape([-1, 1])
-    print("采样点数目：" + str(len(wave_data)))     #输出应为采样点数目
+    print("采样点数目：" + str(len(wave_data)))  # 输出应为采样点数目
     f.close()
 
     return wave_data, params
@@ -196,12 +200,13 @@ def unzip(from_file, to_dir):
     zip_file.close()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import scipy.signal as signal
 
     # 读取音频信号
     # unzip("./dataset/raw/ren1.zip", "./dataset/unzip/")
-    wave_data, params = readWav("./dataset/unzip/ren1/0.wav")
+    wave_data, params = readWav("./dataset/unzip/ren2/5.wav")
+    # wave_data, params = readWav("./record.wav")
 
     # 语音信号分帧加窗
     N = 256         # 一帧时间 = N / framerate, 得 N 的范围: 160-480, 取最近2的整数次方 256
@@ -217,7 +222,8 @@ if __name__=="__main__":
     zerocrossingrate = calZeroCrossingRate(frames, N).reshape(1, num_frame)
 
     # 端点检测
-    endpoint = detectEndPoint(wave_data, energy[0], zerocrossingrate[0])    # 利用短时能量
+    endpoint = detectEndPoint(
+        wave_data, energy[0], zerocrossingrate[0])    # 利用短时能量
     # endpoint = detectEndPoint(wave_data, amplitude[0], zerocrossingrate[0]) # 利用短时幅度
 
     sorted_endpoint = sorted(set(endpoint))
@@ -225,7 +231,6 @@ if __name__=="__main__":
     plot_wave(wave_data, endpoint, N, M)
 
     # 输出为 wav 格式
-    # [TODO: 规范化输出格式利于后续分类工作]
     store = []
     writeWav(store, "1", "0", wave_data, sorted_endpoint, params, N, M)
     df = pd.DataFrame(store, columns=['wave_data', 'person_id', 'content'])
