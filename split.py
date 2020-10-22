bc261472a1b5380b196e0a1b92ce36fda532d07c
wind2375like <wind2375like@163.com>
2020-10-22 19:36:56 +0800

    filter out invalid data

--- split.py
+++ split.py
@@ -39,6 +39,7 @@ for window_type in ["rect", "hamming", "hanning"]:
     store = []
     for ren in os.listdir(unzip_path):
         person_id = int(ren[3:])
+        if person_id == 4 or person_id == 32 or person_id == 6 or person_id == 9 or person_id > 100 : continue
         if not isinstance(person_id, int):
             raise_error("error at {}. not int person id\n".format(ren))
             continue
